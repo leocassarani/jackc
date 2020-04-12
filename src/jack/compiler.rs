6236de58ce22c953aaf7e98a7258074af81af506
Leo Cassarani <me@leocassarani.net>
2020-04-13 00:00:57 +0100

    Add the ability to compile the main function in ConvertToBin.jack

--- src/jack/compiler.rs
+++ src/jack/compiler.rs
@@ -20,11 +20,13 @@ impl Compiler {
     }
 
     fn compile_subroutine(&self, sub: &Subroutine) -> Vec<vm::Command> {
-        let name = format!("{}.{}", self.class.name, sub.name).to_ascii_lowercase();
+        let name = format!("{}.{}", self.class.name, sub.name);
+        let locals = sub.body.vars.len() as u16;
 
-        let func = vm::Command::Function(name, 0); // TODO: number of locals
+        let func = vm::Command::Function(name, locals); // TODO: number of locals
 
         let body = sub.body.statements.iter().flat_map(|st| match st {
+            Statement::Let { lhs, index, rhs } => self.compile_let(lhs, index.as_ref(), rhs),
             Statement::Do(call) => self.compile_do(call),
             Statement::Return(value) => self.compile_return(value.as_ref()),
             _ => unimplemented!(),
@@ -33,22 +35,16 @@ impl Compiler {
         iter::once(func).chain(body).collect()
     }
 
+    fn compile_let(&self, _lhs: &str, _index: Option<&Expr>, rhs: &Expr) -> Vec<vm::Command> {
+        let mut cmds = self.compile_expr(rhs);
+        cmds.push(vm::Command::Pop(vm::Segment::Local, 0));
+        cmds
+    }
+
     fn compile_do(&self, call: &SubroutineCall) -> Vec<vm::Command> {
-        let args = call.args.iter().flat_map(|arg| self.compile_expr(arg));
-
-        let mut name = match call.receiver.as_ref() {
-            Some(class) => format!("{}.", class.to_ascii_lowercase()),
-            _ => String::new(),
-        };
-
-        name.push_str(&call.subroutine.to_ascii_lowercase());
-
-        let jump = vec![
-            vm::Command::Call(name, 1),
-            vm::Command::Pop(vm::Segment::Temp, 0),
-        ];
-
-        args.chain(jump).collect()
+        let mut cmds = self.compile_subroutine_call(call);
+        cmds.push(vm::Command::Pop(vm::Segment::Temp, 0));
+        cmds
     }
 
     fn compile_return(&self, value: Option<&Expr>) -> Vec<vm::Command> {
@@ -60,13 +56,26 @@ impl Compiler {
         vec![push, vm::Command::Return]
     }
 
+    fn compile_subroutine_call(&self, call: &SubroutineCall) -> Vec<vm::Command> {
+        let args = call.args.iter().flat_map(|arg| self.compile_expr(arg));
+
+        let mut name = match call.receiver.as_ref() {
+            Some(class) => format!("{}.", class),
+            _ => String::new(),
+        };
+        name.push_str(&call.subroutine);
+
+        let jump = iter::once(vm::Command::Call(name, call.args.len() as u16));
+        args.chain(jump).collect()
+    }
+
     fn compile_expr(&self, expr: &Expr) -> Vec<vm::Command> {
         match expr {
             Expr::Term(term) => self.compile_term(term),
             Expr::Binary(op, left, right) => {
                 let mut cmds = self.compile_term(left);
                 cmds.extend(self.compile_expr(right).into_iter());
-                cmds.push(self.compile_op(*op));
+                cmds.push(self.compile_binary_op(*op));
                 cmds
             }
         }
@@ -75,16 +84,30 @@ impl Compiler {
     fn compile_term(&self, term: &Term) -> Vec<vm::Command> {
         match term {
             Term::IntConst(n) => vec![vm::Command::Push(vm::Segment::Constant, *n as u16)],
+            Term::Var(_name) => vec![vm::Command::Push(vm::Segment::Local, 0)],
+            Term::SubroutineCall(call) => self.compile_subroutine_call(call),
             Term::Bracketed(expr) => self.compile_expr(expr),
+            Term::Unary(op, subterm) => {
+                let mut cmds = self.compile_term(subterm);
+                cmds.push(self.compile_unary_op(*op));
+                cmds
+            }
             _ => unimplemented!(),
         }
     }
 
-    fn compile_op(&self, op: BinaryOp) -> vm::Command {
+    fn compile_binary_op(&self, op: BinaryOp) -> vm::Command {
         match op {
             BinaryOp::Add => vm::Command::Add,
-            BinaryOp::Multiply => vm::Command::Call("math.multiply".to_string(), 2),
+            BinaryOp::Multiply => vm::Command::Call("Math.multiply".to_string(), 2),
             _ => unimplemented!(),
+        }
+    }
+
+    fn compile_unary_op(&self, op: UnaryOp) -> vm::Command {
+        match op {
+            UnaryOp::Minus => vm::Command::Neg,
+            UnaryOp::Not => vm::Command::Not,
         }
     }
 }
--- tests/compiler_test.rs
+++ tests/compiler_test.rs
@@ -19,13 +19,42 @@ fn seven_jack_test() {
     assert_eq!(
         compiler.compile(),
         vec![
-            Command::Function("main.main".into(), 0),
+            Command::Function("Main.main".into(), 0),
             Command::Push(Segment::Constant, 1),
             Command::Push(Segment::Constant, 2),
             Command::Push(Segment::Constant, 3),
-            Command::Call("math.multiply".into(), 2),
+            Command::Call("Math.multiply".into(), 2),
             Command::Add,
-            Command::Call("output.printint".into(), 1),
+            Command::Call("Output.printInt".into(), 1),
+            Command::Pop(Segment::Temp, 0),
+            Command::Push(Segment::Constant, 0),
+            Command::Return,
+        ]
+    );
+}
+
+#[test]
+fn convert_to_bin_jack_test() {
+    let source = read_test_file("ConvertToBin.jack");
+    let tokenizer = Tokenizer::new(&source);
+    let class = Parser::new(tokenizer).parse().expect("parsing error");
+    let compiler = Compiler::new(class);
+
+    assert_eq!(
+        compiler.compile(),
+        vec![
+            Command::Function("Main.main".into(), 1),
+            Command::Push(Segment::Constant, 8001),
+            Command::Push(Segment::Constant, 16),
+            Command::Push(Segment::Constant, 1),
+            Command::Neg,
+            Command::Call("Main.fillMemory".into(), 3),
+            Command::Pop(Segment::Temp, 0),
+            Command::Push(Segment::Constant, 8000),
+            Command::Call("Memory.peek".into(), 1),
+            Command::Pop(Segment::Local, 0),
+            Command::Push(Segment::Local, 0),
+            Command::Call("Main.convert".into(), 1),
             Command::Pop(Segment::Temp, 0),
             Command::Push(Segment::Constant, 0),
             Command::Return,
