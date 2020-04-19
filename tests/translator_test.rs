6f28d59f4e056adc1b31c49dd223e1b6c3826236
Leo Cassarani <me@leocassarani.net>
2020-04-19 20:46:09 +0100

    Add support for translating the PointerTest sample program

--- tests/translator_test.rs
+++ tests/translator_test.rs
@@ -65,3 +65,35 @@ fn basic_test() {
     assert_eq!(emulator.ram.get(3012), 42);
     assert_eq!(emulator.ram.get(3015), 45);
 }
+
+#[test]
+fn pointer_test() {
+    let translator = Translator::new(&[
+        Command::Push(Segment::Constant, 3030),
+        Command::Pop(Segment::Pointer, 0),
+        Command::Push(Segment::Constant, 3040),
+        Command::Pop(Segment::Pointer, 1),
+        Command::Push(Segment::Constant, 32),
+        Command::Pop(Segment::This, 2),
+        Command::Push(Segment::Constant, 46),
+        Command::Pop(Segment::That, 6),
+        Command::Push(Segment::Pointer, 0),
+        Command::Push(Segment::Pointer, 1),
+        Command::Add,
+        Command::Push(Segment::This, 2),
+        Command::Sub,
+        Command::Push(Segment::That, 6),
+        Command::Add,
+    ]);
+
+    let rom = translator.translate();
+    let mut emulator = Emulator::new(&rom);
+    emulator.ram.init(&[(0, 256)]);
+    emulator.run(450);
+
+    assert_eq!(emulator.ram.get(3), 3030);
+    assert_eq!(emulator.ram.get(4), 3040);
+    assert_eq!(emulator.ram.get(256), 6084);
+    assert_eq!(emulator.ram.get(3032), 32);
+    assert_eq!(emulator.ram.get(3046), 46);
+}
