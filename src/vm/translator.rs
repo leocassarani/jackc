da1dad8fbf84d786922ac17578ec783afa4e3065
Leo Cassarani <me@leocassarani.net>
2020-04-28 20:42:15 +0100

    Tidy up the translation of the EQ, GT, LT comparison operations

--- src/vm/translator.rs
+++ src/vm/translator.rs
@@ -2,6 +2,99 @@ use super::command::*;
 use crate::asm;
 use crate::asm::Instruction;
 
+fn eq() -> Vec<Instruction> {
+    vec![
+        asm!(("EQ")),
+        asm!(@"R14"),
+        asm!(M = D),
+        asm!(@"SP"),
+        asm!(AM = M - 1),
+        asm!(D = M),
+        asm!(A = A - 1),
+        asm!(D = M - D),
+        asm!(M = 0),
+        asm!(@"END_EQ"),
+        asm!(D;JNE),
+        asm!(@"SP"),
+        asm!(A = M - 1),
+        asm!(M = -1),
+        asm!(("END_EQ")),
+        asm!(@"R14"),
+        asm!(A = M),
+        asm!(0;JMP),
+    ]
+}
+
+fn lt() -> Vec<Instruction> {
+    vec![
+        asm!(("LT")),
+        asm!(@"R15"),
+        asm!(M = D),
+        asm!(@"SP"),
+        asm!(AM = M - 1),
+        asm!(D = M),
+        asm!(A = A - 1),
+        asm!(D = M - D),
+        asm!(M = 0),
+        asm!(@"END_LT"),
+        asm!(D;JGE),
+        asm!(@"SP"),
+        asm!(A = M - 1),
+        asm!(M = -1),
+        asm!(("END_LT")),
+        asm!(@"R15"),
+        asm!(A = M),
+        asm!(0;JMP),
+    ]
+}
+
+fn gt() -> Vec<Instruction> {
+    vec![
+        asm!(("GT")),
+        asm!(@"R15"),
+        asm!(M = D),
+        asm!(@"SP"),
+        asm!(AM = M - 1),
+        asm!(D = M),
+        asm!(A = A - 1),
+        asm!(D = M - D),
+        asm!(M = 0),
+        asm!(@"END_GT"),
+        asm!(D;JLE),
+        asm!(@"SP"),
+        asm!(A = M - 1),
+        asm!(M = -1),
+        asm!(("END_GT")),
+        asm!(@"R15"),
+        asm!(A = M),
+        asm!(0;JMP),
+    ]
+}
+
+fn temp_register(idx: u16) -> Instruction {
+    match idx {
+        0 => asm!(@"R5"),
+        1 => asm!(@"R6"),
+        2 => asm!(@"R7"),
+        3 => asm!(@"R8"),
+        4 => asm!(@"R9"),
+        5 => asm!(@"R10"),
+        6 => asm!(@"R11"),
+        7 => asm!(@"R12"),
+        _ => panic!("invalid temp segment offset: {}", idx),
+    }
+}
+
+fn segment_register(seg: Segment) -> Instruction {
+    match seg {
+        Segment::Argument => asm!(@"ARG"),
+        Segment::Local => asm!(@"LCL"),
+        Segment::This => asm!(@"THIS"),
+        Segment::That => asm!(@"THAT"),
+        _ => panic!("unexpected segment: {}", seg),
+    }
+}
+
 pub struct Translator<'a> {
     cmds: &'a [Command],
     count: usize,
@@ -13,65 +106,11 @@ impl<'a> Translator<'a> {
     }
 
     pub fn translate(&mut self) -> Vec<Instruction> {
-        let mut prog = vec![
-            asm!(@"START"),
-            asm!(0;JMP),
-            asm!(("EQ")),
-            asm!(@"R14"),
-            asm!(M = D),
-            asm!(@"SP"),
-            asm!(AM = M - 1),
-            asm!(D = M),
-            asm!(A = A - 1),
-            asm!(D = M - D),
-            asm!(M = 0),
-            asm!(@"END_EQ"),
-            asm!(D;JNE),
-            asm!(@"SP"),
-            asm!(A = M - 1),
-            asm!(M = -1),
-            asm!(("END_EQ")),
-            asm!(@"R14"),
-            asm!(A = M),
-            asm!(0;JMP),
-            asm!(("LT")),
-            asm!(@"R15"),
-            asm!(M = D),
-            asm!(@"SP"),
-            asm!(AM = M - 1),
-            asm!(D = M),
-            asm!(A = A - 1),
-            asm!(D = M - D),
-            asm!(M = 0),
-            asm!(@"END_LT"),
-            asm!(D;JGE),
-            asm!(@"SP"),
-            asm!(A = M - 1),
-            asm!(M = -1),
-            asm!(("END_LT")),
-            asm!(@"R15"),
-            asm!(A = M),
-            asm!(0;JMP),
-            asm!(("GT")),
-            asm!(@"R15"),
-            asm!(M = D),
-            asm!(@"SP"),
-            asm!(AM = M - 1),
-            asm!(D = M),
-            asm!(A = A - 1),
-            asm!(D = M - D),
-            asm!(M = 0),
-            asm!(@"END_GT"),
-            asm!(D;JLE),
-            asm!(@"SP"),
-            asm!(A = M - 1),
-            asm!(M = -1),
-            asm!(("END_GT")),
-            asm!(@"R15"),
-            asm!(A = M),
-            asm!(0;JMP),
-            asm!(("START")),
-        ];
+        let mut prog = vec![asm!(@"START"), asm!(0;JMP)];
+        prog.extend(eq());
+        prog.extend(lt());
+        prog.extend(gt());
+        prog.push(asm!(("START")));
         prog.extend(self.cmds.iter().flat_map(|cmd| self.translate_cmd(cmd)));
         prog
     }
@@ -84,33 +123,14 @@ impl<'a> Translator<'a> {
             Command::And => self.translate_binary_op(asm!(M = D & M)),
             Command::Or => self.translate_binary_op(asm!(M = D | M)),
             Command::Not => self.translate_unary_op(asm!(M = !M)),
-            Command::Eq => self.translate_eq(),
-            Command::Gt => self.translate_gt(),
-            Command::Lt => self.translate_lt(),
+            Command::Eq => self.translate_comparison(asm!(@"EQ")),
+            Command::Gt => self.translate_comparison(asm!(@"GT")),
+            Command::Lt => self.translate_comparison(asm!(@"LT")),
             Command::Pop(Segment::Pointer, 0) => self.translate_pop(&[asm!(@"THIS")]),
             Command::Pop(Segment::Pointer, 1) => self.translate_pop(&[asm!(@"THAT")]),
-            Command::Pop(Segment::Temp, idx) => {
-                let temp = match idx {
-                    0 => asm!(@"R5"),
-                    1 => asm!(@"R6"),
-                    2 => asm!(@"R7"),
-                    3 => asm!(@"R8"),
-                    4 => asm!(@"R9"),
-                    5 => asm!(@"R10"),
-                    6 => asm!(@"R11"),
-                    7 => asm!(@"R12"),
-                    _ => panic!("invalid temp segment offset: {}", idx),
-                };
-                self.translate_pop(&[temp])
-            }
+            Command::Pop(Segment::Temp, idx) => self.translate_pop(&[temp_register(*idx)]),
             Command::Pop(segment, idx) => {
-                let register = match segment {
-                    Segment::Argument => asm!(@"ARG"),
-                    Segment::Local => asm!(@"LCL"),
-                    Segment::This => asm!(@"THIS"),
-                    Segment::That => asm!(@"THAT"),
-                    _ => panic!("unexpected segment: {}", segment),
-                };
+                let register = segment_register(*segment);
 
                 match idx {
                     0 => self.translate_pop(&[register, asm!(A = M)]),
@@ -143,27 +163,10 @@ impl<'a> Translator<'a> {
                 self.translate_push(&[asm!(@"THAT"), asm!(D = M)])
             }
             Command::Push(Segment::Temp, idx) => {
-                let temp = match idx {
-                    0 => asm!(@"R5"),
-                    1 => asm!(@"R6"),
-                    2 => asm!(@"R7"),
-                    3 => asm!(@"R8"),
-                    4 => asm!(@"R9"),
-                    5 => asm!(@"R10"),
-                    6 => asm!(@"R11"),
-                    7 => asm!(@"R12"),
-                    _ => panic!("invalid temp segment offset: {}", idx),
-                };
-                self.translate_push(&[temp, asm!(D = M)])
+                self.translate_push(&[temp_register(*idx), asm!(D = M)])
             }
             Command::Push(segment, idx) => {
-                let register = match segment {
-                    Segment::Argument => asm!(@"ARG"),
-                    Segment::Local => asm!(@"LCL"),
-                    Segment::This => asm!(@"THIS"),
-                    Segment::That => asm!(@"THAT"),
-                    _ => panic!("unexpected segment: {}", segment),
-                };
+                let register = segment_register(*segment);
 
                 match idx {
                     0 => self.translate_push(&[register, asm!(A = M), asm!(D = M)]),
@@ -201,40 +204,14 @@ impl<'a> Translator<'a> {
         ]
     }
 
-    fn translate_eq(&mut self) -> Vec<Instruction> {
+    fn translate_comparison(&mut self, op: Instruction) -> Vec<Instruction> {
         let label = format!("RET_ADDRESS_{}", self.count);
         self.count += 1;
 
         vec![
             asm!(@label.clone()),
             asm!(D = A),
-            asm!(@"EQ"),
-            asm!(0;JMP),
-            asm!((label)),
-        ]
-    }
-
-    fn translate_gt(&mut self) -> Vec<Instruction> {
-        let label = format!("RET_ADDRESS_{}", self.count);
-        self.count += 1;
-
-        vec![
-            asm!(@label.clone()),
-            asm!(D = A),
-            asm!(@"GT"),
-            asm!(0;JMP),
-            asm!((label)),
-        ]
-    }
-
-    fn translate_lt(&mut self) -> Vec<Instruction> {
-        let label = format!("RET_ADDRESS_{}", self.count);
-        self.count += 1;
-
-        vec![
-            asm!(@label.clone()),
-            asm!(D = A),
-            asm!(@"LT"),
+            op,
             asm!(0;JMP),
             asm!((label)),
         ]
